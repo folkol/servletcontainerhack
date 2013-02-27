99aa0fa765d69adba03080775da911d02b3ea6d8
Mattias Johansson <mattias.johansson@atex.com>
2013-02-27 14:23:31 +0100

    Basic administration, such as jar manifest configuration and .gitignore

---
+++ src/main/java/com/folkol/WebServer.java
@@ -0,0 +1,10 @@
+package com.folkol;
+
+public class WebServer
+{
+    public static void main( String[] args )
+    {
+        // Launches the server, will run until interrupted
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/folkol/WebserverTest.java
@@ -0,0 +1,12 @@
+package com.folkol;
+
+import junit.framework.TestCase;
+
+public class WebserverTest
+    extends TestCase
+{
+    public void testNothing()
+    {
+        assertTrue( true );
+    }
+}
